4de23178552c5b73b3a539ef70ad409306052505
Lorik Mucolli <l.mucolli@jacobs-university.de>
2017-08-11 15:24:21 +0200

    Add files via upload

---
+++ marioPython/create_training_data.py
@@ -0,0 +1,23 @@
+import numpy as np
+from grabscreen import grab_screen
+import cv2
+import time
+from getkeys import key_check
+import os
+
+
+def keys_to_output(keys):
+    '''
+    Convert keys to a ...multi-hot... array
+
+    [A,W,D] boolean values.
+    '''
+    output = [0,0,0]
+
+    if 'A' in keys:
+        output[0] = 1
+    elif 'D' in keys:
+        output[2] = 1
+    else:
+        output[1] = 1
+    return output
